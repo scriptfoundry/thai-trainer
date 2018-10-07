217829755476baa4330cfdea7b0e7d1d1489aee0
Andrew Adamson <andrew.adamson@scriptfoundry.com>
2018-10-07 14:32:12 +0900

    fixing wonky progress bar

--- src/components/Quiz/Progress.js
+++ src/components/Quiz/Progress.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Progress = ({ index, queue }) => {
-    const style={ width: `${ Math.floor(100 * index / queue.length) }%` };
+    const style={ width: `${ Math.floor(1000 * index / queue.length) / 10 }%` };
     const className = `progress stage-${ Math.floor( 3 * index / queue.length ) }`;
     return <div className={ className }><div style={style } className="bar"></div></div>;
 };
